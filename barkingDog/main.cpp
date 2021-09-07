f68888d725f547d2ea021a5a53859b8789841caf
smilehae <mihaepyun@gmail.com>
2021-09-08 01:35:21 +0900

    백준 11652 다시 : 기존 배열만 이용

--- barkingDog/main.cpp
+++ barkingDog/main.cpp
@@ -1,27 +1,44 @@
 #include <iostream>
-#include <map>
+#include <algorithm>
 
 using namespace std;
 long long arr[100002];
-map<long long, int> myMap;
+int curNum=0;
  int mx = 0;
+ int cnt=0;
  long long maxIndex = 0;
 
 int main(){
+        ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int n;
     cin>>n;
     for(int i=0;i<n;i++){
         cin>>arr[i];
-        myMap[arr[i]]++;
+    }
+    sort(arr,arr+n);
+    maxIndex = arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]!=curNum){
+            cnt = 1;
+            curNum = arr[i];
+            cout<<"it's not same. now cur Num "<<curNum<<"\n";
+
+        }
+        else{
+            cnt++;
+             cout<<"it's same num. count is "<<cnt<<" and max is "<<mx<<"\n";
+
+            if(cnt > mx){
+                mx = cnt;
+                maxIndex = arr[i];
+                cout<<"num is chaned to "<<maxIndex<<"\n";
+
+            }
+        }
+
     }
 
-    for(map<long long,int>::iterator it=myMap.begin();it!=myMap.end();it++){
-       
-        if(it->second>mx){
-            mx = it->second;
-            maxIndex = it->first;
-        }
-    }
 
     cout<<maxIndex;
 
