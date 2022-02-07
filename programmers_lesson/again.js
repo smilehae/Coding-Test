e76d6eb5c2e438a85788550e5c6022f89354f848
smilehae <mihaepyun@gmail.com>
2022-02-08 03:40:11 +0900

    map,set관련 내용 추가

--- programmers_lesson/again.js
+++ programmers_lesson/again.js
@@ -38,6 +38,7 @@ for (const key in obj2) {
   //   console.log(key, obj2[key]);
 }
 
+/*
 //연결 리스트
 //구현해야 하는 것 : 가장 끝에 append. 특정 value를 find, 특정 value를 remove, 특정 위치에 특정 값을 insert
 class Node {
@@ -110,3 +111,63 @@ linkedList.remove(3);
 linkedList.display();
 linkedList.insert(linkedList.find(2), 10);
 linkedList.display();
+
+*/
+
+//큐. 배열로, 선형 큐로 구현
+// class Queue {
+//   constructor() {
+//     this.front = 0;
+//     this.rear = 0;
+//     this.queue = [];
+//     this.size = 0;
+//   }
+//   enqueue(value) {
+//     this.queue[this.rear++] = value;
+//     this.size++;
+//   }
+//   dequeue() {
+//     if (this.size <= 0) return -1;
+//     const returnVal = this.queue[this.front++];
+//     this.size--;
+//     return returnVal;
+//   }
+// }
+
+// const queue = new Queue();
+// queue.enqueue(2);
+// queue.enqueue(3);
+// console.log(queue.dequeue());
+// console.log(queue.dequeue());
+
+// Map은 key.value 페어를 배열처럼 저장하는 것.
+const myMap = new Map();
+myMap.set("Mihae", "5123-3231");
+myMap.set("hahae", "1234-1231");
+myMap.set("Mihae", "3232-3232");
+
+console.log(myMap);
+console.log(myMap.get("Mihae"));
+console.log(myMap.size);
+
+for (let [key, value] of myMap) {
+  console.log(key, value);
+}
+
+for (const item of myMap) {
+  console.log(item[0], item[1]);
+}
+// set은 특정 값을 넣어두는 것. 존재 여부를 판단한다.
+const mySet = new Set();
+mySet.add(1);
+mySet.add(5);
+mySet.delete(1);
+console.log(mySet.has(1));
+const ob = { a: 1, b: 2 };
+mySet.add(ob);
+console.log(mySet);
+console.log(mySet.has(ob));
+for (let item of mySet) {
+  console.log(item);
+}
+mySet.forEach((item) => console.log(item));
