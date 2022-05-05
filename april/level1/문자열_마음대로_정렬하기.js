f17b94c9fb35f9456e96e7735c37e842b250437d
smilehae <mihaepyun@gmail.com>
2022-05-05 23:00:11 +0900

    정렬_문자열마음대로_프로그래머스

---
+++ april/level1/문자열_마음대로_정렬하기.js
@@ -0,0 +1,13 @@
+function solution(strings, n) {
+  strings.sort((a, b) => {
+    if (a[n] !== b[n]) {
+      if (a[n] > b[n]) {
+        return 1;
+      } else return -1;
+    } else {
+      if (a > b) return 1;
+      else return -1;
+    }
+  });
+  return strings;
+}
